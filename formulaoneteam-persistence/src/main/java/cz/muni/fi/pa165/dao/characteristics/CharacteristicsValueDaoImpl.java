4d142ba2934f11cca3b027378be4408d4316e4a8
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-10-29 15:02:18 +0100

    Add database access object for CharacteristicsValue
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

---
+++ formulaoneteam-persistence/src/main/java/cz/muni/fi/pa165/dao/characteristics/CharacteristicsValueDaoImpl.java
@@ -0,0 +1,46 @@
+package cz.muni.fi.pa165.dao.characteristics;
+
+import cz.muni.fi.pa165.entity.CharacteristicsType;
+import cz.muni.fi.pa165.entity.CharacteristicsValue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class CharacteristicsValueDaoImpl implements CharacteristicsValueDao {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public CharacteristicsValue findById(Long id) {
+        return entityManager.find(CharacteristicsValue.class, id);
+    }
+
+    @Override
+    public void add(CharacteristicsValue entity) {
+        entityManager.persist(entity);
+    }
+
+    @Override
+    public void delete(CharacteristicsValue entity) {
+        entityManager.remove(findById(entity.getId()));
+    }
+
+    @Override
+    public void update(CharacteristicsValue entity) {
+        entityManager.merge(entity);
+    }
+
+    @Override
+    public List<CharacteristicsValue> findAll() {
+        return entityManager.createQuery("select characteristics from CharacteristicsValue characteristics", CharacteristicsValue.class)
+                .getResultList();
+    }
+
+    @Override
+    public List<CharacteristicsValue> findCharacteristicValuesByType(CharacteristicsType type) {
+        return entityManager.createQuery("select characteristics from CharacteristicsValue characteristics where characteristics.type = :type", CharacteristicsValue.class)
+                .setParameter("type", type)
+                .getResultList();
+    }
+}
