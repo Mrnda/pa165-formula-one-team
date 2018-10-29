b26968539e2af7451fd070ecd7a35132dead525f
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-10-29 14:41:00 +0100

    Add Driver entity

--- formulaoneteam-persistence/src/main/java/cz/muni/fi/pa165/entity/Driver.java
+++ formulaoneteam-persistence/src/main/java/cz/muni/fi/pa165/entity/Driver.java
@@ -1,4 +1,75 @@
 package cz.muni.fi.pa165.entity;
 
-public class Driver {
+import cz.muni.fi.pa165.entity.base.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Driver extends User {
+
+    @NotNull
+    @Column(nullable = false)
+    private String nationality;
+
+    @Basic
+    @Temporal(TemporalType.DATE)
+    @NotNull
+    private Date birthdate;
+
+    @Column
+    private DriverStatus driverStatus;
+
+    @OneToMany
+    private Set<CharacteristicsValue> characteristics = new HashSet<>();
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public DriverStatus getDriverStatus() {
+        return driverStatus;
+    }
+
+    public void setDriverStatus(DriverStatus driverStatus) {
+        this.driverStatus = driverStatus;
+    }
+
+    public Set<CharacteristicsValue> getCharacteristics() {
+        return Collections.unmodifiableSet(characteristics);
+    }
+
+    public void addCharacteristic(CharacteristicsValue characteristicsValue){
+        characteristics.add(characteristicsValue);
+    }
+
+    public void removeCharacteristics(CharacteristicsValue characteristicsValue){
+        characteristics.remove(characteristicsValue);
+    }
+
+    @Override
+    public String toString() {
+        return "Driver{" +
+                "nationality='" + getNationality() + '\'' +
+                ", birthdate=" + getBirthdate() +
+                ", driverStatus=" + getDriverStatus() +
+                ", characteristics=" + getCharacteristics() +
+                "} " + super.toString();
+    }
 }
