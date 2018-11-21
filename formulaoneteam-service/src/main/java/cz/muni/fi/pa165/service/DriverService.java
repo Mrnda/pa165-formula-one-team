73984863be7081918243c5aa3cc539a9d84c220f
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-11-21 20:48:34 +0100

    Add DriverService
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

---
+++ formulaoneteam-service/src/main/java/cz/muni/fi/pa165/service/DriverService.java
@@ -0,0 +1,17 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.entity.Driver;
+import cz.muni.fi.pa165.enums.DriverStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface DriverService {
+    void registerDriver(Driver driver, String unencryptedPassword);
+    boolean authenticate(Driver driver, String password);
+    Driver findDriverById(long id);
+    Driver findDriverByEmail(String email);
+    List<Driver> getAllDrivers();
+    List<Driver> getAllDriversByStatus(DriverStatus status);
+}
