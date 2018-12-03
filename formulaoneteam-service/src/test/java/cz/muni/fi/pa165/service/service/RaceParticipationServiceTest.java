ef5bb9ccda39a324c6eee0d623052eafee8471c4
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-12-03 08:29:13 +0100

    Add tests for world championship participations
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

--- formulaoneteam-service/src/test/java/cz/muni/fi/pa165/service/service/RaceParticipationServiceTest.java
+++ formulaoneteam-service/src/test/java/cz/muni/fi/pa165/service/service/RaceParticipationServiceTest.java
@@ -1,20 +1,30 @@
 package cz.muni.fi.pa165.service.service;
 
 import cz.muni.fi.pa165.dao.RaceParticipation.RaceParticipationDao;
+import cz.muni.fi.pa165.entity.CarSetup;
+import cz.muni.fi.pa165.entity.Driver;
+import cz.muni.fi.pa165.entity.Race;
 import cz.muni.fi.pa165.entity.RaceParticipation;
+import cz.muni.fi.pa165.enums.DriverStatus;
 import cz.muni.fi.pa165.service.RaceParticipationServiceImpl;
+import cz.muni.fi.pa165.service.RaceService;
 import cz.muni.fi.pa165.service.base.BaseServiceTest;
+import cz.muni.fi.pa165.service.date.DateService;
 import cz.muni.fi.pa165.service.exceptions.FormulaOneTeamException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.util.Pair;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 /**
  * @author Adel Chakouri
@@ -23,6 +33,12 @@ public class RaceParticipationServiceTest extends BaseServiceTest<RaceParticipat
 
     @Mock
     private RaceParticipationDao dao;
+
+    @Mock
+    private RaceService raceServiceMock;
+
+    @Mock
+    private DateService dateServiceMock;
 
     @InjectMocks
     private RaceParticipationServiceImpl raceParticipationService;
@@ -105,6 +121,80 @@ public class RaceParticipationServiceTest extends BaseServiceTest<RaceParticipat
         Assert.assertNotNull(resultRaceList);
         Assert.assertEquals(resultRaceList.size(), 2);
         Assert.assertTrue(resultRaceList.contains(entity));
+    }
+
+    @Test(expected = FormulaOneTeamException.class)
+    public void participateInWorldChampionship_championshipInPast_throws() {
+        //Given
+        final Date date = createDate(2, 11, 1987);
+        final String location = "Barcelona";
+        final Pair<CarSetup, Driver> firstDriverCarSetup = Pair.of(createCarSetup(), createDriver());
+        final Pair<CarSetup, Driver> secondDriverCarSetup = Pair.of(createCarSetup(), createDriver());
+        when(dateServiceMock.getCurrentDate()).thenReturn(createDate(2, 11, 2018));
+
+        //When
+        final List<RaceParticipation> raceParticipations = raceParticipationService.participateInWorldChampionship(date, location, firstDriverCarSetup, secondDriverCarSetup);
+    }
+
+    @Test(expected = FormulaOneTeamException.class)
+    public void participateInWorldChampionship_withTestDriver_throws() {
+        //Given
+        final Date date = createDate(2, 12, 2018);
+        final String location = "Barcelona";
+        final Driver testDriver = createDriver();
+        testDriver.setDriverStatus(DriverStatus.TEST);
+        final Pair<CarSetup, Driver> firstDriverCarSetup = Pair.of(createCarSetup(), testDriver);
+        final Pair<CarSetup, Driver> secondDriverCarSetup = Pair.of(createCarSetup(), createDriver());
+        when(dateServiceMock.getCurrentDate()).thenReturn(createDate(2, 11, 2018));
+
+        //When
+        final List<RaceParticipation> raceParticipations = raceParticipationService.participateInWorldChampionship(date, location, firstDriverCarSetup, secondDriverCarSetup);
+    }
+
+    @Test
+    public void participateInWorldChampionship_withValidData_createsRace() {
+        //Given
+        final Date date = createDate(2, 12, 2018);
+        final String location = "Barcelona";
+        final Pair<CarSetup, Driver> firstDriverCarSetup = Pair.of(createCarSetup(), createDriver());
+        final Pair<CarSetup, Driver> secondDriverCarSetup = Pair.of(createCarSetup(), createDriver());
+        when(dateServiceMock.getCurrentDate()).thenReturn(createDate(2, 11, 2018));
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(2018, Calendar.DECEMBER, 2);
+        when(dateServiceMock.createCalendarForDate(date)).thenReturn(calendar);
+
+        //When
+        final List<RaceParticipation> raceParticipations = raceParticipationService.participateInWorldChampionship(date, location, firstDriverCarSetup, secondDriverCarSetup);
+
+        //Then
+        final Race race = new Race(date, "2018 world championship", location);
+        verify(raceServiceMock, times(1)).add(race);
+    }
+
+    @Test
+    public void participateInWorldChampionship_withValidData_createsParticipations() {
+        //Given
+        final Date date = createDate(2, 12, 2018);
+        final String location = "Barcelona";
+        final Pair<CarSetup, Driver> firstDriverCarSetup = Pair.of(createCarSetup(), createDriver());
+        final Pair<CarSetup, Driver> secondDriverCarSetup = Pair.of(createCarSetup(), createDriver());
+        when(dateServiceMock.getCurrentDate()).thenReturn(createDate(2, 11, 2018));
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(2018, Calendar.DECEMBER, 2);
+        when(dateServiceMock.createCalendarForDate(date)).thenReturn(calendar);
+
+        //When
+        final List<RaceParticipation> raceParticipations = raceParticipationService.participateInWorldChampionship(date, location, firstDriverCarSetup, secondDriverCarSetup);
+
+        //Then
+        final Race race = new Race(date, "2018 world championship", location);
+        final RaceParticipation firstParticipation = new RaceParticipation(firstDriverCarSetup.getFirst(),
+                firstDriverCarSetup.getSecond(),
+                race,
+                RaceParticipation.NO_RESULT_POSITION);
+        verify(dao, times(2)).add(firstParticipation);
+        assertEquals(2, raceParticipations.size());
+        assertTrue(raceParticipations.contains(firstParticipation));
     }
 
     @Override
