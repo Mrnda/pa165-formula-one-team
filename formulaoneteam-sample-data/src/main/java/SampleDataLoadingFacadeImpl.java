6810fcacc7d5bf43e8bcb625d225b5a83ca76e25
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-12-05 20:41:00 +0100

    Fix missing component registration for SampleDataLoadingFacade
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

--- formulaoneteam-sample-data/src/main/java/SampleDataLoadingFacadeImpl.java
+++ formulaoneteam-sample-data/src/main/java/SampleDataLoadingFacadeImpl.java
@@ -1,8 +1,14 @@
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @author mrnda (Michal Mrnuštík)
  */
 
+@Component
+@Transactional
 public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade {
+
     @Override
     public void loadData() {
 
