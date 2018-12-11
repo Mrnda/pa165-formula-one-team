b2a99b561e4bfd1a5dea148211c81f2670a06055
Michal Mrnustik <michal.mrnustik@outlook.com>
2018-12-11 17:11:20 +0100

    Remove T prefix from generics parameter
    Signed-off-by: Michal Mrnustik <michal.mrnustik@outlook.com>

--- formulaoneteam-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/base/BaseCrudController.java
+++ formulaoneteam-rest/src/main/java/cz/muni/fi/pa165/rest/controllers/base/BaseCrudController.java
@@ -16,18 +16,19 @@ import java.util.List;
  * @author mrnda (Michal Mrnuštík)
  */
 
-public abstract class BaseCrudController<TFacade extends BaseEntityFacade<TDTO, TEntity>, TDTO extends BaseDTO, TEntity extends BaseEntity> {
+public abstract class BaseCrudController<Facade extends BaseEntityFacade<DTO, Entity>, DTO extends BaseDTO, Entity extends BaseEntity> {
 
     @Inject
-    TFacade facade;
+    Facade facade;
 
     @RequestMapping
-    public @ResponseBody List<TDTO> getAll() {
+    public @ResponseBody
+    List<DTO> getAll() {
         return facade.getAll();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TDTO> findById(@PathVariable long id) {
+    public ResponseEntity<DTO> findById(@PathVariable long id) {
         try {
             return ResponseEntity.ok(facade.findById(id));
         } catch (EntityNotFoundException e) {
@@ -38,7 +39,7 @@ public abstract class BaseCrudController<TFacade extends BaseEntityFacade<TDTO, 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> remove(@PathVariable long id) {
         try {
-            TDTO dto = facade.findById(id);
+            DTO dto = facade.findById(id);
             facade.remove(dto);
             return ResponseEntity.ok().build();
         } catch (EntityNotFoundException e) {
@@ -47,9 +48,9 @@ public abstract class BaseCrudController<TFacade extends BaseEntityFacade<TDTO, 
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TDTO> add(@RequestBody TDTO dto) {
+    public ResponseEntity<DTO> add(@RequestBody DTO dto) {
         try {
-            TDTO createdDTO = facade.add(dto);
+            DTO createdDTO = facade.add(dto);
             return ResponseEntity.ok(createdDTO);
         } catch (FormulaOneTeamException e) {
             return ResponseEntity.unprocessableEntity().build();
@@ -57,7 +58,7 @@ public abstract class BaseCrudController<TFacade extends BaseEntityFacade<TDTO, 
     }
 
     @RequestMapping(value = "/", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TDTO> update(@RequestBody TDTO updatedDto) {
+    public ResponseEntity<DTO> update(@RequestBody DTO updatedDto) {
         try {
             facade.update(updatedDto);
             return ResponseEntity.ok(facade.findById(updatedDto.getId()));
