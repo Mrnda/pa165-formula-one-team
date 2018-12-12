d1a4ebdb76fceb84a66da06b684986a91278604b
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-12-12 13:16:55 +0100

    Add navigation bar
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

--- formulaoneteam-rest/src/main/java/cz/muni/fi/pa165/rest/ApiUris.java
+++ formulaoneteam-rest/src/main/java/cz/muni/fi/pa165/rest/ApiUris.java
@@ -4,7 +4,7 @@ package cz.muni.fi.pa165.rest;
  * @author mrnda (Michal Mrnuštík)
  */
 
-public class ApiUris {
+public abstract class ApiUris {
     public static final String ROOT_URI_RACE = "/race";
     public static final String ROOT_URI_TEST_DRIVE = "/test-drive";
     public static final String ROOT_URI_RACE_PARTICIPATION = "/race-participation";
