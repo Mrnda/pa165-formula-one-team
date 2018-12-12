85433e347e0fbd3c65715c9b6b6965c312106361
Michal Mrnuštík <michal.mrnustik@outlook.com>
2018-12-12 22:19:49 +0100

    Add author annotation to controllers
    Signed-off-by: Michal Mrnuštík <michal.mrnustik@outlook.com>

--- formulaoneteam-web/src/main/java/cz/muni/fi/pa165/mvc/controllers/CarsController.java
+++ formulaoneteam-web/src/main/java/cz/muni/fi/pa165/mvc/controllers/CarsController.java
@@ -3,6 +3,9 @@ package cz.muni.fi.pa165.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author mrnda (Michal Mrnuštík)
+ */
 @Controller
 @RequestMapping("/cars")
 public class CarsController {
