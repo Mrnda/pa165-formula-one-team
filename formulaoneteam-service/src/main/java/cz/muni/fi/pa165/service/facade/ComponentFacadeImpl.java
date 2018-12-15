1b3dd24aa65185d2137ffbb0bf78ea114ab79dc8
Michal Mrnustik <michal.mrnustik@outlook.com>
2018-12-15 19:44:33 +0100

    Add parameter handling to the ComponentFacade
    Signed-off-by: Michal Mrnustik <michal.mrnustik@outlook.com>

--- formulaoneteam-service/src/main/java/cz/muni/fi/pa165/service/facade/ComponentFacadeImpl.java
+++ formulaoneteam-service/src/main/java/cz/muni/fi/pa165/service/facade/ComponentFacadeImpl.java
@@ -1,12 +1,17 @@
 package cz.muni.fi.pa165.service.facade;
 
 import cz.muni.fi.pa165.dto.ComponentDTO;
+import cz.muni.fi.pa165.dto.ComponentParameterDTO;
 import cz.muni.fi.pa165.entity.Component;
+import cz.muni.fi.pa165.entity.ComponentParameter;
 import cz.muni.fi.pa165.facade.ComponentFacade;
+import cz.muni.fi.pa165.service.ComponentParameterService;
 import cz.muni.fi.pa165.service.ComponentService;
 import cz.muni.fi.pa165.service.facade.base.BaseEntityFacadeImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
 
 /**
  * @author Théo Desjardins
@@ -17,6 +22,9 @@ public class ComponentFacadeImpl
         extends BaseEntityFacadeImpl<ComponentDTO, Component, ComponentService>
         implements ComponentFacade {
 
+    @Inject
+    private ComponentParameterService componentParameterService;
+
     @Override
     protected Class<ComponentDTO> getDtoClass() {
         return ComponentDTO.class;
@@ -25,5 +33,28 @@ public class ComponentFacadeImpl
     @Override
     protected Class<Component> getEntityClass() {
         return Component.class;
+    }
+
+    @Override
+    public void addParameter(long componentId, ComponentParameterDTO parameter) {
+        ComponentParameter parameterEntity
+                = componentParameterService.add(beanMappingService.mapTo(parameter, ComponentParameter.class));
+        Component component = service.findById(componentId);
+        component.addParameter(parameterEntity);
+        service.update(component);
+    }
+
+    @Override
+    public void updateParameter(ComponentParameterDTO parameterDTO) {
+        componentParameterService.update(beanMappingService.mapTo(parameterDTO, ComponentParameter.class));
+    }
+
+    @Override
+    public void removeParameter(long componentId, ComponentParameterDTO parameter) {
+        ComponentParameter componentParameter = componentParameterService.findById(parameter.getId());
+        Component component = service.findById(componentId);
+        component.removeParameter(componentParameter);
+        service.update(component);
+        componentParameterService.remove(parameter.getId());
     }
 }
